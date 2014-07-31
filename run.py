143f3f4104c1496ec66a3acb45687052e209f072
jensj <jensj@1cd22e71-7009-4eff-a670-84da8a3a38ae>
2014-07-31 08:18:24 +0000

    Download db-files
    git-svn-id: https://svn.fysik.dtu.dk/projects/cmr2/trunk@11 1cd22e71-7009-4eff-a670-84da8a3a38ae

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
+from __future__ import print_function
+import os
+import urllib
 from ase.utils.sphinx import create_png_files
+
+url = 'https://wiki.fysik.dtu.dk/cmr2/_downloads/'
+projects = ['dssc']
+
+for name in projects:
+    path = os.path.join(name, name + '.db')
+    if not os.path.isfile(path):
+        print('Dowloading', path)
+        urllib.urlretrieve(url + name + '.db', path)
+        
 create_png_files(run_all_python_files=True, exclude=['./run.py', './conf.py'])
