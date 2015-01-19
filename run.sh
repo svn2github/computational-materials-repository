6a02ab06623cf2186d28b6b2765bb2cc07d94fc8
jensj <jensj@1cd22e71-7009-4eff-a670-84da8a3a38ae>
2015-01-19 10:37:21 +0000

    buglet
    git-svn-id: https://svn.fysik.dtu.dk/projects/cmr2/trunk@64 1cd22e71-7009-4eff-a670-84da8a3a38ae

--- run.sh
+++ run.sh
@@ -7,13 +7,14 @@ svn up
 # Update CMR:
 cd ../cmr
 svn up
+cd ..
 
 html=html.tar.gz
 
 if [ ase -nt $html ] || [ cmr -nt $html ] || [ db-files -nt $html ]
 then
+    cd cmr
     # Run Python scripts:
-    cd ../cmr
     PYTHONPATH=../ase:$PYTHONPATH python run.py copy
 
     # Build html:
